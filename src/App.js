6b927284d8ad8e7dc62d83d0628d0fd3db71e466
diegofdz <fernandezdiegonahuel@gmail.com>
2022-01-04 21:55:20 -0300

    se modifico texto de index

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 
 import './App.css';
+import Subtitulo from './components/Subtitulo';
+import Titulo from './components/Titulo';
+import "bootstrap/dist/css/bootstrap.min.css"
+import Button from 'react-bootstrap/Button'
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <Titulo></Titulo>
+      <Subtitulo comision="c19i" estado={true}/>
+      <hr/>
+      <Button variant="primary" >Primary</Button>
+  
+    </div>
     
   );
 }
---
+++ src/components/Titulo.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Titulo extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="display-4 text-center my-5">Lista de Tareas</h1>
+      </div>
+    );
+  }
+}
+
+export default Titulo;
+
