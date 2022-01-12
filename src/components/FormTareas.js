d21b9701ecd3776e38c1900ddf3d1ac492be68ff
diegofdz <fernandezdiegonahuel@gmail.com>
2022-01-12 09:28:47 -0300

    se agrego loigca para eliminar producto

--- src/components/FormTareas.js
+++ src/components/FormTareas.js
@@ -1,25 +1,45 @@
-import React from 'react';
+import React, { useState } from "react";
 //import Form from 'react-bootstrap/Form'
 //import Buttom from 'react-bootstrap/Button'
-import {Form, Button} from "react-bootstrap"
-import ListaTareas from './ListaTareas';
-
+import { Form, Button } from "react-bootstrap";
+import ListaTareas from "./ListaTareas";
 
 const FormTareas = () => {
-    return (
-        <div className='container'>
-            <Form>
-        <Form.Group className="mb-3 d-flex" >
-          <Form.Control type="text" placeholder="Ingrese una tarea" />
-          <Button variant="primary" type="submit">
-          Enviar
-        </Button>
+  //aqui va la logica
+  //aqui creo los state
+  const [listaTareas, setListaTareas] = useState([]);
+  const [tarea, setTarea] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // guardar el contenido del state tarea dentro del arreglo listatareas
+    setListaTareas([...listaTareas, tarea]);
+    setTarea("");
+  };
+
+  const borrarTarea = (nombre) =>{
+    //filtramos el state sacando el elemento que quiero borrar
+    let arregloModificado = listaTareas.filter((valor)=> valor !== nombre)
+    //actualizar state
+    setListaTareas(arregloModificado);
+  }
+
+  return (
+    <div className="container">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3 d-flex">
+          <Form.Control
+            type="text"
+            placeholder="Ingrese una tarea"
+            onChange={(e) => setTarea(e.target.value.trimStart())}
+            value={tarea}
+          />
+          <Button variant="primary">Enviar</Button>
         </Form.Group>
       </Form>
-      <ListaTareas></ListaTareas>
-        </div>
-
-    );
+      <ListaTareas arregloTareas={listaTareas} borrarTarea={borrarTarea}></ListaTareas>
+    </div>
+  );
 };
 
 export default FormTareas;
